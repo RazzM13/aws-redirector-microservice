7513c323db5fc50aa7501332054f1e5de8d6ed3c
RazzM13 <34776591+RazzM13@users.noreply.github.com>
2018-03-03 14:04:20 +0000

    Init.

---
+++ README.md
@@ -0,0 +1,7 @@
+# A NodeJS bootstrap scaffold project for a generic REST-based microservice utilizing the AWS technology stack.
+
+## Dosage:
+One repo-clone per microservice.
+
+## Usage:
+Replace helloworld code and references with your own.
---
+++ index.js
@@ -0,0 +1,10 @@
+exports.handler = (event, context, callback) => {
+    // TODO implement
+    let response = {
+        'isBase64Encoded': false,
+        'statusCode': 418,
+        'headers': {},
+        'body': 'Hello World from a Little-Teapot!'
+    }
+    callback(null, JSON.stringify(response));
+};
